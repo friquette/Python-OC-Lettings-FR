d69997e5ae9c5ece1ba2e426fb7e658af1949ad4
Thomas Fricard <thomasfricard@gmail.com>
2022-06-03 12:16:58 +0200

    tests for profiles_index and profile views

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.test import Client
+from django.shortcuts import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from profiles.models import Profile
+
+import pytest
+
+
+@pytest.fixture
+def create_profile(db):
+    user = User.objects.create(
+        username='HeadlinesGazer',
+        first_name='Jamie',
+        last_name='Lal',
+        email='jssssss33@acee9.live'
+    )
+    profile = Profile.objects.create(
+        user=user,
+        favorite_city='Buenos Aires'
+    )
+    return profile
+
+
+@pytest.mark.django_db
+def test_profiles_index_view():
+    client = Client()
+    uri = reverse('profiles:profiles_index')
+    resp = client.get(uri)
+
+    assert resp.status_code == 200
+    assert "<title>Profiles</title>" in str(resp.content)
+
+
+@pytest.mark.django_db
+def test_profile_view(create_profile):
+    client = Client()
+    uri = reverse('profiles:profile', args=('HeadlinesGazer',))
+    resp = client.get(uri)
+
+    assert resp.status_code == 200
+    assert "<title>HeadlinesGazer</title>" in str(resp.content)
