4bd4fb04f080b1d9f72a884870b9d18cc5300015
Thomas Fricard <thomasfricard@gmail.com>
2022-06-17 11:37:59 +0200

    add sentry for errors monitoring

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,5 +1,8 @@
 import os
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -63,13 +66,13 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'oc-lettings-site.sqlite3'),
     }
-}"""
-DATABASES = {
+}
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
@@ -78,7 +81,7 @@ DATABASES = {
         'HOST': 'db',
         'PORT': 5432,
     }
-}
+}"""
 
 
 # Password validation
@@ -118,3 +121,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+sentry_sdk.init(
+    dsn="https://884757fb7b3a4bd5ae7cb3d08b7bf0db@o1290660.ingest.sentry.io/6511743",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,9 +3,15 @@ from django.urls import path, include
 
 from . import views
 
+
+def test(request):
+    division = 1 / 0
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls', namespace='lettings')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('sentry-debug/', test)
 ]
